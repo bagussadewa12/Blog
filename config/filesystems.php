5b65bd512d11be9a1903128253ec78cfd6d9e754
bagussadewa12 <117002631+bagussadewa12@users.noreply.github.com>
2025-08-15 11:34:11 +0800

    new push

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,13 +38,11 @@ return [
             'report' => false,
         ],
 
-        'public' => [
+            'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
         ],
 
         's3' => [
